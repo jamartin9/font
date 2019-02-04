005117cd2d426329059a5647d4a28786f6fb11db
Justin <DoNotEmailMe@email.com>
2019-02-03 23:43:42 -0500

    ansible

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p "$HOME/.local/share/fonts/Unknown Vendor/TrueType/Fira Code/Fira_Code_Regular.ttf"
-mkdir -p "$HOME/.fonts/f/FiraCode_Regular_Symbol.otf"
-cp FiraCode-Regular-Symbol.otf "$HOME/.fonts/f/FiraCode_Regular_Symbol.otf"
-cp FiraCode-Regular-modified.ttf "$HOME/.local/share/fonts/Unknown Vendor/TrueType/Fira Code/Fira_Code_Regular.ttf"
